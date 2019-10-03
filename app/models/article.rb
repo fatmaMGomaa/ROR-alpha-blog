4c49febad82d13affc99879dce97b36d8594a881
fatmaMGomaa <fatma-mohamed@feps.edu.eg>
2019-10-03 17:19:57 +0200

    add user model and make association btn user and article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+    belongs_to :user
     validates :title, presence: true, length: {minimum: 3, maxmum: 50}
     validates :description, presence: true, length: {minimum: 10, maxmum: 300}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190915125939) do
+ActiveRecord::Schema.define(version: 20191003151328) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",       limit: 255
     t.text     "description", limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id",     limit: 4
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username",   limit: 255
+    t.string   "email",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
